776bd1225d5f3af40d549bc6b6d7996166a61a8c
rafaelholland <rafael.holland@bandtec.com.br>
2022-07-27 18:21:05 -0300

    added first 2 concepts of SOLID

---
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -0,0 +1,25 @@
+package br.com.alura.rh.service;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+public class ReajusteService {
+
+    private List<ValidacaoReajuste> validacoes;
+
+    public ReajusteService(List<ValidacaoReajuste> validacoes) {
+        this.validacoes = validacoes;
+    }
+
+    public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento){
+        this.validacoes.forEach(v -> v.validar(funcionario, aumento));
+        BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
+        funcionario.atualizarSalario(salarioReajustado);
+    }
+}
---
+++ src/br/com/alura/rh/service/ValidacaoPeriodicidadeEntreReajustes.java
@@ -0,0 +1,21 @@
+package br.com.alura.rh.service;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class ValidacaoPeriodicidadeEntreReajustes implements ValidacaoReajuste{
+
+    public void validar(Funcionario funcionario, BigDecimal aumento){
+        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+        LocalDate dataAtual = LocalDate.now();
+        long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+        if(mesesDesdeUltimoReajuste < 6){
+            throw new ValidacaoException("Intervalo entre reajustes deve ser maior que 6 meses!");
+        }
+    }
+}
